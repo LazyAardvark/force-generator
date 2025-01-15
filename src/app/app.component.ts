e4e296ef8e323c4d0fcc255000591d17cffe91e8
ben j <ben.jerauld@gmail.com>
2025-01-15 13:59:17 -0500

    Initial Layout stub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/components/layout/footer/footer.component";
+import { HeaderComponent } from './shared/components/layout/header/header.component';
+import { MainContentComponent } from './shared/components/layout/main-content/main-content.component';
+import { SidePanelComponent } from './shared/components/layout/side-panel/side-panel.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'fg-root',
+  imports: [FooterComponent, HeaderComponent, MainContentComponent, SidePanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'force-generator';
 }
---
+++ src/app/shared/components/layout/side-panel/side-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fg-side-panel',
+  imports: [],
+  templateUrl: './side-panel.component.html',
+  styleUrl: './side-panel.component.scss'
+})
+export class SidePanelComponent {
+
+}
