cc42f7095b7c5644964fede55b1a17690fedec91
ben j <ben.jerauld@gmail.com>
2025-01-18 20:33:09 -0500

    roller form basics

--- src/app/shared/components/forms/roller-setup/roller-setup.component.ts
+++ src/app/shared/components/forms/roller-setup/roller-setup.component.ts
@@ -1,43 +1,52 @@
-import {Component, inject} from '@angular/core';
-import {Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
-import { NgIf } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @Component({
   selector: 'fg-roller-setup',
-   imports: [ReactiveFormsModule,
+  imports: [ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
     MatCheckboxModule,
     MatCardModule,
-    NgIf],
+    MatSliderModule,
+  ],
   templateUrl: './roller-setup.component.html',
   styles: '',
-  
- 
+
+
 })
 export class RollerSetupComponent {
 
   private formBuilder = inject(FormBuilder);
-  
+
   forceBuilderForm = this.formBuilder.group({
-      roster: [''],
-      battleValue : ['0', Validators.required],
-      battleValueTolerance: [''],
-      baseSkillLevel:[''],
-      allowOfficers: [''],
-      allowDuplicates: [''],
-    });
-  
+    selectionRoster: ['', Validators.required],
+    battleValue: ['0', Validators.required],
+    battleValueTolerance: [''],
+    baseGunnerySkill: [3],
+    basePilotingSkill: [4],
+    allowOfficers: [''],
+    allowDuplicates: [''],
+  });
+
+  rosters: String[] = [
+    "AGoAC Box",
+    "Beginner Box",
+    "Mercenaries Box",
+    "Clan Invasion Box"
+  ];
+
 
   onSubmit() {
     const formValue = this.forceBuilderForm.value;
-    console.log("Force Generator Configuration Value :" , formValue);
+    console.log("Force Generator Configuration Value :", formValue);
   }
 }
